b24e271ef596f831d02decd68be3eb09d0d9888c
Luke Braithwaite <lukebraithwaite7@gmail.com>
2022-08-29 00:51:42 +0100

    :sparkles: responsive navbar
    Added responsive navbar to NavBar.tsx

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -7,18 +7,27 @@ import Link from 'next/link';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import tailwindConfig from '../../tailwind.config.js';
 import NavBarDesktop from '@components/NavBar/NavBarDesktop';
+import NavBarMobile from '@components/NavBar/NavBarMobile';
+import { useMediaQuery } from 'react-responsive';
 
 const config = resolveConfig(tailwindConfig);
 
 const NavBar = (): JSX.Element => {
+	//@ts-ignore
+	const query = `(min-width: ${config.theme?.screens?.lg})`;
+	console.log(query);
+
+	const matches = useMediaQuery({
+		query,
+	});
 	return (
-		<nav className="sticky top-0 z-50 flex justify-between md:px-20 px-4 py-5 bg-black text-white pt-4 w-full">
+		<nav className="sticky top-0 z-50 flex items-center justify-between md:px-20 px-4 py-5 bg-black text-white pt-4 w-full">
 			<div>
 				<Link href="/" passHref>
 					<a>AspieCoder.com</a>
 				</Link>
 			</div>
-			<NavBarDesktop />
+			{matches ? <NavBarDesktop /> : <NavBarMobile />}
 		</nav>
 	);
 };
---
+++ components/NavBar/NavBarMobile.tsx
@@ -0,0 +1,60 @@
+import { ForwardedRef, forwardRef, ReactNode } from 'react';
+import Link from 'next/link';
+import { Menu, Transition } from '@headlessui/react';
+import { Bars3Icon } from '@heroicons/react/24/solid';
+
+const MyLink = forwardRef<HTMLAnchorElement, any>(
+	(
+		props: { href: string; children: ReactNode; className: string },
+		ref: ForwardedRef<HTMLAnchorElement>
+	) => {
+		let { href, children, ...rest } = props;
+		return (
+			<Link href={href} passHref>
+				<a ref={ref} {...rest}>
+					{children}
+				</a>
+			</Link>
+		);
+	}
+);
+
+MyLink.displayName = 'NextLinkElement';
+
+const Example = (): JSX.Element => {
+	return (
+		<Menu as="div" className="relative inline-block">
+			<Menu.Button
+				as="div"
+				className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+			>
+				<Bars3Icon color="white" className="h-9 w-9" aria-hidden="true" />
+			</Menu.Button>
+			<Transition
+				enter="transition duration-100 ease-out"
+				enterFrom="transform scale-95 opacity-0"
+				enterTo="transform scale-100 opacity-100"
+				leave="transition duration-75 ease-out"
+				leaveFrom="transform scale-100 opacity-100"
+				leaveTo="transform scale-95 opacity-0"
+			>
+				<Menu.Items className="absolute right-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+					<div className="px-1 py-1 flex flex-col items-center">
+						<Menu.Item>
+							<MyLink className="text-black text-xl my-2 mx-10" href="/blog">
+								Blog
+							</MyLink>
+						</Menu.Item>
+						<Menu.Item>
+							<MyLink className="text-black text-xl my-2 mx-10" href="/about">
+								About
+							</MyLink>
+						</Menu.Item>
+					</div>
+				</Menu.Items>
+			</Transition>
+		</Menu>
+	);
+};
+
+export default Example;
