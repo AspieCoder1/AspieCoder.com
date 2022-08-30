77c8c554cb76664bed5e1a6e6de377fbb56a60ad
Luke Braithwaite <lukebraithwaite7@gmail.com>
2022-08-30 20:03:51 +0100

    :lock: add security check for token

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -6,7 +6,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSlugs } from '@utils/contentfulClient';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+	if (req.headers.authorization === process.env.REVALIDATE_SECRET) {
 		return res.status(401).json({ message: 'Invalid token' });
 	}
 
